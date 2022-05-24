c8b03d7bed63242ad862d2f4e03f9410f69b7aef
Maksim <maksme99@gmail.com>
2022-05-25 00:21:32 +0300

    redirect

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Problems from './components/Problems';
 import Main from './components/Main';
 import LogIn from './components/LogIn';
 import Regist from './components/Regist';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, useNavigate} from 'react-router-dom'
+import { useEffect } from 'react';
 
 function App() {
+  // const navigate = useNavigate();
+  // useEffect(()=>{navigate("/main");},[])
+ 
   return (
     <Router>
       <Routes>
         <Route path="/problems" element={<Problems/>}/>
+        <Route path="/" element={<Main/>}/>
         <Route path="/main" element={<Main/>}/>
         <Route path="/login" element={<LogIn/>}/>
         <Route path="/registration" element={<Regist/>}/>
